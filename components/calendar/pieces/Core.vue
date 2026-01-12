1732854bf235d12dcf73ad14110cbc0f36c79a30
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-01-11 17:22:06 -0800

    Extracted out hour lines + added times

--- components/calendar/pieces/Core.vue
+++ components/calendar/pieces/Core.vue
@@ -1,20 +1,15 @@
 <script setup>
 import { currentSchedule, currentSessions } from '../../../composables/useSchedules';
-import { dayStartHour, dayEndHour } from '../../../composables/dayLimits';
 
 import Session from './Session.vue';
+import HourLines from './HourLines.vue';
 
 const selectedSectionCrn = ref("");
-
-var hourRange = dayEndHour - dayStartHour;
 </script>
 
 <template>
   <div class="calendar">
-    <div class="hour-lines">
-      <div class="hour-line" v-for="n in hourRange + 1"></div>
-    </div>
-
+    <HourLines />
     <Session
       v-for="session in currentSessions"
       :key="session.crn"
@@ -26,29 +21,3 @@ var hourRange = dayEndHour - dayStartHour;
     />
   </div>
 </template>
-
-<style scoped>
-  .hour-lines {
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    display: flex;
-    height: 100%;
-  }
-
-  .hour-line {
-    border-top: 1px solid gray;
-    width: 100%;
-  }
-
-  .hour-block {
-    height: 100%;
-    border-style: solid;
-    border-color: gray;
-    border-width: 1px 0px;
-  }
-
-  .v-event-timed {
-    border-radius: 500px !important;
-  }
-</style>
---
+++ components/calendar/pieces/HourLines.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { dayStartHour, dayEndHour } from '../../../composables/dayLimits';
+
+function formatHour(hour) {
+  let meridian = "m";
+  if (hour < 12) {
+    meridian = "a" + meridian;
+  } else {
+    meridian = "p" + meridian;
+  }
+
+  if (hour > 12) {
+    hour -= 12;
+  }
+
+  return `${hour}${meridian}`;
+}
+
+var hourRange = dayEndHour - dayStartHour;
+</script>
+
+<template>
+  <div class="hour-lines">
+    <div class="hour-line" v-for="n in hourRange + 1">
+      <div class="hour-text">
+        {{ formatHour(dayStartHour + n - 1) }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .hour-lines {
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    height: 100%;
+  }
+
+  .hour-line {
+    border-top: 1px solid gray;
+    width: 100%;
+  }
+
+  .hour-text {
+    text-align: right;
+    position: relative;
+    top: -.75em;
+    left: -101%;
+  }
+
+</style>
