dab62f1986e084d5af72dbbb4d7ecdf803d56335
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-01-12 17:07:40 -0800

    added times to results

--- components/search/pieces/Result.vue
+++ components/search/pieces/Result.vue
@@ -7,6 +7,22 @@
     score: Number
   })
 
+  function formatTime(timeStr) {
+    let time = new Date(`1970-01-05T${timeStr}`);
+    return time.toLocaleTimeString();
+  }
+
+  function formatSessions(section) {
+    let sessionsFormatted = [];
+    for (const session of section.sessions) {
+      const daysOfWeekFormatted = session.meeting_time.days_of_week.join("");
+      const start = session.meeting_time.start;
+      const end = session.meeting_time.end;
+      sessionsFormatted.push(`${daysOfWeekFormatted} ${formatTime(start)} - ${formatTime(end)}`);
+    }
+    return sessionsFormatted;
+  }
+
   const section = allCourseSections[props.crn];
 </script>
 
@@ -14,6 +30,7 @@
   <div class="result-container" @click="addClass(props.crn)">
     <h3 class="coursecode">{{ `${section.dept} ${section.course_no}` }} <span class="section-no">{{ section.section_no }}</span></h3>
     <h4>{{ section.title }}</h4>
+    <h5>{{ formatSessions(section).join("; ") }}</h5>
   </div>
 </template>
 
