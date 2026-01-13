99dc7af0e640b91247c1c87b624b1b8fb8b45152
Crystal McArdle-Ventura <162148969+CrystalMV13@users.noreply.github.com>
2026-01-12 16:49:56 -0800

    modified index.vue for better view while testing

--- pages/index.vue
+++ pages/index.vue
@@ -4,16 +4,24 @@
   </div>
 
   <ScheduleChanger />
-  <Search />
+  
+  <div class="search-container">
+    <Search />
+  </div>
 </template>
 
 <style scoped>
   .calendar-container {
-    /* position: absolute; */
-    /* top: 5em; */
-    /* left: 5em; */
+    position: absolute;
+    top: 5em;
+    left: 5em;
     width: 50em;
     height: 50em;
     margin-bottom: 5em;
+  }
+
+  .search-container {
+    position: absolute;
+    top: 60em;
   }
 </style>
